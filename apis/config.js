66250883e3b8f132d5196e4f73a91d3edc83b3d8
Jibin Mathews <jazzyarchitects@users.noreply.github.com>
2018-07-17 11:39:02 +0530

    For old cluster compatibility

--- apis/config.js
+++ apis/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongoUrl: process.env.MONGO_URL || "mongo.default.svc.cluster.local:27017",
     getMongoConnectionString() {
-        return `${process.env.MONGO_URL || "mongo.default.svc.cluster.local:27017"}`
+        return `${process.env.MONGO_URL || "mongodb://mongo.default.svc.cluster.local:27017"}`
     }
 }
