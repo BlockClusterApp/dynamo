f7d5ace4282aed283a22766970439b24a663bc43
Narayan Prusty <arsenal.narayan@gmail.com>
2018-07-17 19:42:52 +0400

    Added -L to screen

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@ then
   	pkill screen
 	screen -d -m ./constellation.sh
 	sleep 5
-	screen -d -m ./quorum-node.sh > quroum-node.log
-	screen -d -m node apis/app.js > app.log
+	screen -L -d -m ./quorum-node.sh
+	screen -L -d -m node apis/app.js
 	sleep 10;
-    screen -d -m node apis/init.js > init.log
+    screen -L -d -m node apis/init.js
 	while true;
 	do
 		REMOTEHOST=127.0.0.1
@@ -51,10 +51,10 @@ then
   	pkill screen
 	screen -d -m ./constellation.sh $1
 	sleep 5
-	screen -d -m ./quorum-node.sh $2 $3 > quroum-node.log
-	screen -d -m node apis/app.js > app.log
+	screen -L -d -m ./quorum-node.sh $2 $3
+	screen -L -d -m node apis/app.js
 	sleep 10;
-    screen -d -m node apis/init.js > init.log
+    screen -L -d -m node apis/init.js
 	while true;
 	do
 		REMOTEHOST=127.0.0.1
@@ -99,10 +99,10 @@ then
   	pkill screen
 	screen -d -m ./constellation.sh $1
 	sleep 5
-	screen -d -m ./quorum-node.sh $2 $3 $4 > quroum-node.log
-	screen -d -m node apis/app.js > app.log
+	screen -L -d -m ./quorum-node.sh $2 $3 $4
+	screen -L -d -m node apis/app.js
 	sleep 10;
-    screen -d -m node apis/init.js > init.log
+    screen -L -d -m node apis/init.js
 	while true;
 	do
 		REMOTEHOST=127.0.0.1
