ca61affdd69c97b3eacddadc052760060bb0f15b
Narayan Prusty <arsenal.narayan@gmail.com>
2018-08-07 19:33:49 +0400

    Reset files

--- quorum-node.sh
+++ quorum-node.sh
@@ -96,6 +96,7 @@ $2
 EOF
 geth --datadir ./bcData/node init ./bcData/node/genesis.json
 rm ./bcData/node/nodekey
+echo '[]' >./bcData/node/permissioned-nodes.json
 PRIVATE_CONFIG=./bcData/cnode/constellation.conf geth --datadir ./bcData/node --mine --port 23000 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcapi "admin,debug,eth,miner,net,personal,shh,txpool,web3,istanbul" --rpcport 8545  --ipcpath "geth.ipc" --gasprice 0 --targetgaslimit 999999999999999999 --permissioned --verbosity 2 &> /dynamo/bcData/geth.log
 fi
 fi
