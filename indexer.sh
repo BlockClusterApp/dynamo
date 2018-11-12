d556e18707c8f09cad976fcbe561fb25fb4d7d30
Narayan Prusty <arsenal.narayan@gmail.com>
2018-11-12 18:03:56 +0400

    Changed interval for testing

--- indexer.sh
+++ indexer.sh
@@ -1,1 +1,1 @@
-mongodynamicindexer -d mongodb://localhost:27017/admin -c -p 2 -i 86400 -m 100 --simple -r 7
+mongodynamicindexer -d mongodb://localhost:27017/admin -c -p 2 -i 2 -m 100 --simple -r 7
