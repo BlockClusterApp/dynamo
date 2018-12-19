12848fc6fb93740245cf02aeceb20b83d2e3fc76
Jibin <jibinmathews7@gmail.com>
2018-12-19 16:46:35 +0530

    Adding timestamp for log revisioning

--- apis/ecosystem.config.js
+++ apis/ecosystem.config.js
@@ -1,3 +1,5 @@
+const timestamp = new Date().getTime();
+
 module.exports = {
   apps: [
     {
@@ -6,16 +8,16 @@ module.exports = {
       watch: false,
       instances: 2,
       exec_mode: 'cluster',
-      log_file: '/dynamo/bcData/combined.app.log',
-      out_file: '/dynamo/bcData/app-stdout.log',
-      error_file: '/dynamo/bcData/app-stderr.log'
+      log_file: `/dynamo/bcData/combined.app.${timestamp}.log`,
+      out_file: `/dynamo/bcData/app-stdout.${timestamp}.log`,
+      error_file: `/dynamo/bcData/app-stderr.${timestamp}.log`
     },
     {
       name: 'scanner',
       script: '/dynamo/apis/scanner.js',
-      log_file: '/dynamo/bcData/combined.scanner.log',
-      out_file: '/dynamo/bcData/scanner-stdout.log',
-      error_file: '/dynamo/bcData/scanner-stderr.log'
+      log_file: `/dynamo/bcData/combined.scanner.${timestamp}.log`,
+      out_file: `/dynamo/bcData/scanner-stdout.${timestamp}.log`,
+      error_file: `/dynamo/bcData/scanner-stderr.${timestamp}.log`
     }
   ]
 };
